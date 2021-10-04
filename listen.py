1a68ef0afb213fcdcb17707b50892bfbdaff7953
nicoSix <nicolasix@outlook.fr>
2021-10-04 21:25:44 +0200

    Bot now supports every collection on DE

--- listen.py
+++ listen.py
@@ -1,37 +1,38 @@
 import time
 import requests
+import json
 
 last_listing_nb = 0
-sleep_time = 30
-base_url = "https://us-central1-digitaleyes-prod.cloudfunctions.net/offers-retriever?collection=Solarians&price=asc"
+sleep_time = 15
+base_url = "https://us-central1-digitaleyes-prod.cloudfunctions.net/offers-retriever?price=asc&collection="
 
-def check_solarian(r):
+def check_attribute(r, attr):
   for o in r['offers']:
     for a in o['metadata']['attributes']:
-      if ((o['price'] < 12600000000 and (a['value'] == "Prospector" or a['value'] == "Monitor")) or 
-      (o['price'] < 14600000000 and (a['value'] == "Pilot" or a['value'] == "Spy")) or 
-      (o['price'] < 6600000000 and (a['value'] == "Drone" or a['value'] == "Administrator")) or 
-      (o['price'] < 8600000000 and (a['value'] == "Squad Leader" or a['value'] == "Guard"))):
+      if ((o['price'] < attr['value']) and (a['value'] == attr['attribute'])):
         print("-------------------------------------------")
         print("Alert: %s at %.2f!" %(a['value'], (o['price'] / 1000000000)))
         print("https://digitaleyes.market/item/%s" %(o['mint']))
         print("-------------------------------------------")
 
+def get_attributes():
+  with open('listen_for.json') as json_file:
+    return json.load(json_file)
+
+attributes = get_attributes()
+
 while True:
-  print('GM, checking if there are new listings ...')
-  r = requests.get(base_url).json()
-  # conflict with namespace count, workaround below
-  r['nb_offers'] = r.pop('count')
-  if r['nb_offers'] != last_listing_nb:
-    print("New listings, checking ...")
+  print('GM, checking for good deals ...')
+  for attr in attributes:
+    r = requests.get(base_url + attr['collection']).json()
+    # conflict with namespace count, workaround below
+    r['nb_offers'] = r.pop('count')
     last_listing_nb = r['nb_offers']
-    check_solarian(r)
+    check_attribute(r, attr)
     # must improve that to check until a defined stop price
-    for i in range(0, 10):
+    for i in range(0, 3):
       next_cursor = r['next_cursor']
-      r = requests.get(base_url + "&cursor=" + next_cursor).json()
-      check_solarian(r)
-  else:
-    print("No new offers.")
+      r = requests.get(base_url + attr['collection'] + "&cursor=" + next_cursor + "&collection=").json()
+      check_attribute(r, attr)
   print("Sleeping for %d seconds, GN!" %(sleep_time))
   time.sleep(sleep_time)
