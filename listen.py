0f863b1a41006fef3096fbb8511aecdc0b4ecb14
nicoSix <nicolasix@outlook.fr>
2021-10-04 21:56:48 +0200

    Improving bot speed and accuracy

--- listen.py
+++ listen.py
@@ -9,7 +9,7 @@ base_url = "https://us-central1-digitaleyes-prod.cloudfunctions.net/offers-retri
 def check_attribute(r, attr):
   for o in r['offers']:
     for a in o['metadata']['attributes']:
-      if ((o['price'] < attr['value']) and (a['value'] == attr['attribute'])):
+      if ((o['price'] < attr['value'])):
         print("-------------------------------------------")
         print("Alert: %s at %.2f!" %(a['value'], (o['price'] / 1000000000)))
         print("https://digitaleyes.market/item/%s" %(o['mint']))
@@ -24,15 +24,10 @@ attributes = get_attributes()
 while True:
   print('GM, checking for good deals ...')
   for attr in attributes:
-    r = requests.get(base_url + attr['collection']).json()
+    r = requests.get(base_url + attr['collection'] + "&" + attr['attribute_name'] + "=" + attr['attribute_value']).json()
     # conflict with namespace count, workaround below
     r['nb_offers'] = r.pop('count')
     last_listing_nb = r['nb_offers']
     check_attribute(r, attr)
-    # must improve that to check until a defined stop price
-    for i in range(0, 3):
-      next_cursor = r['next_cursor']
-      r = requests.get(base_url + attr['collection'] + "&cursor=" + next_cursor + "&collection=").json()
-      check_attribute(r, attr)
   print("Sleeping for %d seconds, GN!" %(sleep_time))
   time.sleep(sleep_time)
